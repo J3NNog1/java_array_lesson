20e98781ac49d56d8997a45ca4fd35b44ce0948e
jennifer j <jenniferjuarezjj@gmail.com>
2024-01-09 11:50:56 -0500

    use linear search for randomArray lesson

--- src/RandomArray.java
+++ src/RandomArray.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class RandomArray {
@@ -8,8 +9,31 @@ public class RandomArray {
         for ( int num : firstArray) {
             System.out.println(num + " ");
         }
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
 
-    }
+        int [] secondArray = new int [10];
+        System.out.println(Arrays.toString(secondArray));
+        Arrays.fill(secondArray, 5);
+        System.out.println(Arrays.toString(secondArray));
+
+        int[] thirdArray = getRandomArray(10);
+        System.out.println(Arrays.toString(thirdArray));
+
+        int[] fourthArray =  Arrays.copyOf(thirdArray, thirdArray.length);
+        System.out.println(Arrays.toString(fourthArray));
+
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(fourthArray));
+
+        int[] smallerArray = Arrays.copyOf(thirdArray, 5);
+        System.out.println(Arrays.toString(smallerArray));
+
+        int[] largerArray = Arrays.copyOf(thirdArray, 15);
+        System.out.println(Arrays.toString(largerArray));
+
+            }
     private static int[] getRandomArray(int length) {
         Random random = new Random();
         int[] newInt = new int[length];
